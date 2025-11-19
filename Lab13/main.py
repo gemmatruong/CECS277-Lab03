7e7b1a62d3779fa541d3f2f4ee9a595e83e292c8
gemmatruong <gemmatruong99@gmail.com>
2025-11-18 22:50:52 -0800

    feat: Add function get_date() to get date type input and return in required format

--- Lab13/main.py
+++ Lab13/main.py
@@ -11,3 +11,20 @@ def main_menu():
     print("6. Save and quit")
     choice = check_input.get_int_range("Enter choice: ", 1, 6)
     return choice
+
+def get_date():
+    # Get user's input for month, day, and year.
+    # Return the date in the format: MM/DD/YYYY
+    mm = check_input.get_int_range("Enter month: ", 1, 12)
+    dd = check_input.get_int_range("Enter day: ", 1, 31)
+    yyyy = check_input.get_int_range("Enter year: ", 2000, 2100)
+
+    if mm < 10 and dd < 10:
+        return f"0{mm}/0{dd}/{yyyy}"
+    elif mm < 10:
+        return f"0{mm}/{dd}/{yyyy}"
+    elif dd < 10:
+        return f"{mm}/0{dd}/{yyyy}"
+    else:
+        return f"{mm}/{dd}/{yyyy}"
+
